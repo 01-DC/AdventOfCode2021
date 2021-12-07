96e0dc2746ec3857407484723ce506d5da868063
Devanshu Chatterjee <devanshu4190@gmail.com>
2021-12-08 02:17:17 +0530

    add Day4 Part2

---
+++ Day4/Part2.py
@@ -0,0 +1,41 @@
+# full_input= open("test.txt").read().strip().split('\n')
+full_input= open("input.txt").read().strip().split('\n')
+draw_order= full_input[0].split(',')
+matrices= [[line.split() for line in full_input[pos:pos+5]] for pos in range(2, len(full_input), 6)]
+
+def mark(matrix, number):
+    for r in range(5):
+        for c in range(5):
+            if matrix[r][c]==number:
+                matrix[r][c]= 'X'
+                return matrix
+    return matrix
+
+def checkBingo(matrix):
+    for r in matrix:
+        if r.count('X')==5:
+            return True
+        
+    for x in range(5):
+        if([r[x] for r in matrix].count('X')==5):
+            return True
+    
+    return False
+
+def calcSum(matrix):
+    return sum([sum([int(num) for num in row if num!='X']) for row in matrix])
+
+all_sums= []
+for draw in draw_order:
+    for matrix in matrices:
+        if(checkBingo(matrix)):
+            continue
+
+        matrix= mark(matrix, draw)
+        # print(matrix)
+        if(checkBingo(matrix)):
+            unmarked_sum= calcSum(matrix)
+            all_sums.append(unmarked_sum*int(draw))
+
+print(all_sums[0])
+print(all_sums[-1])
