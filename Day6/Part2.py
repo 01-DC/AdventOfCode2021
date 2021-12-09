02668935c66797edc7b7c7856d6fa8268b59f472
Devanshu Chatterjee <devanshu4190@gmail.com>
2021-12-09 13:27:47 +0530

    add Day6 Part2(fast algo)

--- Day6/Part2.py
+++ Day6/Part2.py
@@ -1,12 +1,16 @@
-# fishes= list(map(int, open('test.txt').read().strip().split(',')))
-fishes= list(map(int, open('input.txt').read().strip().split(',')))
-for day in range(80):
-    new_fishes= []
-    for index, fish in enumerate(fishes):
-        if fish==0:
-            fishes[index]= 7
-            new_fishes.append(8)
-        fishes[index]-= 1
-    fishes.extend(new_fishes)
-    # print(day, fishes)
-print(len(fishes))
+# starting_input= list(map(int, open('test.txt').read().strip().split(',')))
+starting_input= list(map(int, open('input.txt').read().strip().split(',')))
+current_fishes= [0 for i in range(9)]
+for fish in starting_input:
+    current_fishes[fish]+= 1
+
+for day in range(256):
+    dead_fishes= current_fishes[0]
+    # for i in range(8):
+    #     current_fishes[i]= current_fishes[i+1]
+    current_fishes= [current_fishes[i+1] for i in range(8)]
+    current_fishes.append(0)
+    current_fishes[6]+= dead_fishes
+    current_fishes[8]= dead_fishes
+print(sum(current_fishes))
+
