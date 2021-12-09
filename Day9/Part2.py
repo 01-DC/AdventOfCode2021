43ccc06eaf87d6fa571320d4e2e0703bc01406a4
Devanshu Chatterjee <devanshu4190@gmail.com>
2021-12-09 18:32:07 +0530

    add Day9 Part2

--- Day9/Part2.py
+++ Day9/Part2.py
@@ -1,17 +1,21 @@
-full_input= open('test.txt').read().strip().split()
-# full_input= open('input.txt').read().strip().split()
+# full_input= open('test.txt').read().strip().split()
+full_input= open('input.txt').read().strip().split()
 heatmap= []
 for line in full_input:
     heatmap.append(list(map(int, list(line))))
 
 ROWS= len(heatmap)
 COLUMNS= len(heatmap[0])
+dp= [[False for c in range(COLUMNS)] for r in range(ROWS)]
 directions= [(1,0), (0,1), (-1,0), (0,-1)]
 basin_size= []
 
 def checkPoint(r, c):
     if heatmap[r][c]==9:
         return 0
+    if dp[r][c]:
+        return 0
+    dp[r][c]= True
     val= 1
     for dx,dy in directions:
         x,y = c+dx, r+dy
@@ -30,5 +34,5 @@ for r in range(ROWS):
         if min_val > heatmap[r][c]:
             basin_size.append(checkPoint(r, c))
 
-basin_size.sort()
-print(basin_size)
+basin_size.sort(reverse=True)
+print(basin_size[0]*basin_size[1]*basin_size[2])
